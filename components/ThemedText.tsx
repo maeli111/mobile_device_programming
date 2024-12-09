96dfdf1e2d2c288459138c3ac3d5fa757b4169f7
mattd <matt.davidson0513@gmail.com>
2024-12-09 15:21:06 +0100

    Add product focntion with stripe

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -5,7 +5,7 @@ import { useThemeColor } from '@/hooks/useThemeColor';
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link' | 'titleSmall' | 'error' | 'purpleBold';
 };
 
 export function ThemedText({
@@ -26,6 +26,9 @@ export function ThemedText({
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
         type === 'subtitle' ? styles.subtitle : undefined,
         type === 'link' ? styles.link : undefined,
+        type === 'titleSmall' ? styles.titleSmall : undefined,
+        type === 'error' ? styles.error : undefined,
+        type === 'purpleBold'? styles.purpleBold : undefined,
         style,
       ]}
       {...rest}
@@ -34,9 +37,23 @@ export function ThemedText({
 }
 
 const styles = StyleSheet.create({
+  small: {
+    fontSize: 14,
+    lineHeight: 24,
+  },
   default: {
     fontSize: 16,
     lineHeight: 24,
+  },
+  purpleBold: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: 'bold',
+    color: '#635DF6'
+  },
+  error: {
+    fontSize: 14,
+    color: '#db3d3d'
   },
   defaultSemiBold: {
     fontSize: 16,
@@ -48,9 +65,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     lineHeight: 32,
   },
+  titleSmall: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    lineHeight: 32,
+  },
   subtitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: 400,
+    color: 'grey'
   },
   link: {
     lineHeight: 30,
---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyDtyaZSYo68d2zS2XQO6jsuoMJaY87B7iY",
+    authDomain: "nomadescape-77796.firebaseapp.com",
+    projectId: "nomadescape-77796",
+    storageBucket: "nomadescape-77796.firebasestorage.app",
+    messagingSenderId: "1079669499689",
+    appId: "1:1079669499689:web:d69b74e2e3924e2a7a3938",
+    measurementId: "G-QS4HL28G62"
+  };
