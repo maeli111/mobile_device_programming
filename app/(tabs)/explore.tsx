b918e06dbd4bb708c4eacee194d5998da7fa9372
mattd <matt.davidson0513@gmail.com>
2024-12-10 14:11:20 +0100

    Add paiementt focntion with stripe

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,109 +1,153 @@
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, SafeAreaView, View, useColorScheme, Pressable, FlatList, ActivityIndicator, Alert } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import React, { useState } from 'react';
+import Product from '@/components/Product';
+import { collection, DocumentData, getDocs, getFirestore } from 'firebase/firestore'
+import { firebaseConfig } from '../../firebaseConfig.js'
+import { initializeApp } from 'firebase/app';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { CustomerSheetBeta, initPaymentSheet, presentPaymentSheet, StripeProvider } from '@stripe/stripe-react-native';
+import { publicKey } from '@/constants/StripePublicKey';
+
+function ProductList() {
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  const getProducts = async () => {
+    const res = await getDocs(collection(db, "products"));
+
+    let prods: DocumentData[] = [];
+    res.forEach(document => {
+      prods.push({ id: document.id, data: document.data() });
+    });
+
+    return prods;
+  }
+
+  const getPaymentIntent = async (price: number, productID: string) => {
+    const response = await fetch(`https://getpaymentintent-olknvoqq3q-uc.a.run.app/?price=${price}&currency=eur&productID=${productID}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const { paymentIntent, ephemeralKey, customer, displayName } = await response.json();
+
+    return {
+      paymentIntent,
+      ephemeralKey,
+      customer,
+      displayName
+    };
+  };
+
+  const initializePaymentSheet = async (price: number, productID: string) => {
+    const {
+      paymentIntent,
+      ephemeralKey,
+      customer,
+      displayName
+    } = await getPaymentIntent(price, productID);
+
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: displayName,
+      customerId: customer,
+      customerEphemeralKeySecret: ephemeralKey,
+      paymentIntentClientSecret: paymentIntent,
+      allowsDelayedPaymentMethods: true,
+    });
+
+    if (error) {
+      console.log(error);
+      Alert.alert('Error', 'An error has occurred while setting up your payment. Please try again later.');
+    }
+  };
+
+  const openPaymentSheet = async (price: number, productID: string) => {
+    if (!isPaymentActive) { //only one payment can go on at a time
+      setPaymentActive(true);
+      setPaymentID(productID);
+
+      await initializePaymentSheet(price, productID);
+      const { error } = await presentPaymentSheet();
+
+      if (error) {
+        console.log(error);
+        Alert.alert(`Error code: ${error.code}`, error.message);
+      } else {
+        Alert.alert('Success', 'Your order is confirmed!');
+      }
+
+      setPaymentActive(false);
+      setPaymentID(null);
+    }
+  };
+
+  const [isPaymentActive, setPaymentActive] = useState(false);
+  const [paymentActiveProductID, setPaymentID] = useState<string | null>(null);
+
+  const { data, error, isLoading, refetch, isPending } = useQuery({ queryKey: ['products'], queryFn: getProducts });
+
+  return (
+    <View style={styles.productsContainer}>
+      {isPending && isLoading && <ActivityIndicator />}
+      {!isPending && <FlatList refreshing={isLoading} onRefresh={() => refetch()} data={data} keyExtractor={item => item.id} renderItem={({ item }) => <Product activePayment={paymentActiveProductID} productID={item.id} onPress={async () => await openPaymentSheet(item.data.price, item.id)} productName={item.data.name} productPrice={item.data.price / 100} productDescription='This is a description.' />} />}
+    </View>
+  );
+}
 
 export default function TabTwoScreen() {
+  const theme = useColorScheme() ?? 'light';
+  const queryClient = new QueryClient();
+
+  const [ephemeralKey, setEphemeralKey] = useState('');
+  const [customerID, setCustomerID] = useState('');
+  const [showEditPayment, setShowEditPayment] = useState(false);
+
+  const displayEditPaymentInfo = async () => {
+    const response = await fetch(`https://getephemeralsecret-olknvoqq3q-uc.a.run.app`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const { ephemeralKey, customer } = await response.json();
+
+    setEphemeralKey(ephemeralKey);
+    setCustomerID(customer);
+    setShowEditPayment(true);
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <StripeProvider publishableKey={publicKey}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.headingContainer}>
+          <ThemedText type="title">Your Products</ThemedText>
+          <Pressable onPress={async () => await displayEditPaymentInfo()}><ThemedText type="purpleBold">Edit Payment Info</ThemedText></Pressable>
+        </View>
+        <CustomerSheetBeta.CustomerSheet visible={showEditPayment} onResult={() => setShowEditPayment(false)} customerId={customerID} customerEphemeralKeySecret={ephemeralKey}></CustomerSheetBeta.CustomerSheet>
+        <View>
+          <QueryClientProvider client={queryClient}>
+            <ProductList></ProductList>
+          </QueryClientProvider>
+        </View>
+      </SafeAreaView>
+    </StripeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
+  container: {
+    display: 'flex',
+    marginVertical: 70,
+    marginHorizontal: 30,
   },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
+  productsContainer: {
+    marginTop: 30,
+    marginBottom: 30
   },
+  headingContainer: {
+    gap: 3
+  }
 });
